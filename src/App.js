efbd686db637433fc02225f5894f9f8d967c3e47
Bruno Roboredo <roboredo.bruno@gmail.com>
2020-04-21 22:55:15 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Card from './components/Card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        characters: []
+    };
+
+    componentDidMount() {
+        this.getCharacters();
+    }
+
+
+    handleEvent = characterId => {
+        this.state.characters.map(character => {
+            if (character.id === characterId) {
+                this.vote(character.id);
+            }
+        });
+    };
+
+    handleRefresh = () => {
+        // this.getCharacters()
+    }
+
+    render() {
+        const cards = this.state.characters.map(character => (
+            <div className="col-6">
+                <Card key={character.id}
+                      id={character.id}
+                      name={character.name}
+                      sitcomName={character.sitcom.name}
+                      img={character.img}
+                      onVote={this.handleEvent}/>
+            </div>
+        ));
+
+        return <div className="container">
+            <div className="row">
+                {cards}
+            </div>
+            {/*<div className="row">
+                <a href="#" onRefresh={this.handleRefresh()}>Refresh</a>
+            </div>*/}
+        </div>
+    }
+
+    vote(characterId) {
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({character_id: characterId})
+        };
+        fetch('http://localhost:8000/api/vote', requestOptions)
+            .then(async response => {
+                const data = await response.json();
+
+                // check for error response
+                if (!response.ok) {
+                    // get error message from body or default to response status
+                    const error = (data && data.message) || response.status;
+                    return Promise.reject(error);
+                }
+
+
+                this.getCharacters();
+            })
+            .catch(error => {
+                this.setState({errorMessage: error});
+                console.error('There was an error!', error);
+            });
+    }
+
+    getCharacters() {
+        const requestOptions = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+        fetch('http://localhost:8000/api/characters', requestOptions)
+            .then(async response => {
+                const data = await response.json();
+
+                // check for error response
+                if (!response.ok) {
+                    // get error message from body or default to response status
+                    const error = (data && data.message) || response.status;
+                    return Promise.reject(error);
+                }
+
+                this.setState({characters: data});
+            })
+            .catch(error => {
+                this.setState({errorMessage: error});
+                console.error('There was an error!', error);
+            });
+    }
 }
 
 export default App;
