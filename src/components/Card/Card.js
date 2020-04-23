84cf8ac794f790e768b322b0c79c855f767c60c8
Bruno Roboredo <roboredo.bruno@gmail.com>
2020-04-23 18:44:34 +0100

    added cursor pointer and buttons

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -7,12 +7,16 @@ class Card extends Component {
 
     render() {
         return (
-            <div className="Card text-center" onClick={this.handleClick}>
+            <div className="Card text-center c-pointer" onClick={this.handleClick}>
                 <section className="showcase">
                     <section className="nes-container with-title">
                         <h3 className="title">{this.props.name}</h3>
                         <h6 className="subtitle">{this.props.sitcomName}</h6>
                         <img srcSet={ this.props.img } style={{maxHeight: '250px'}} className="img-fluid" />
+
+                        <div className="justify-content-center">
+                            <button type="button" className="nes-btn is-primary btn-sm mt-3">Escolher</button>
+                        </div>
                     </section>
                 </section>
             </div>
